8f3689fd8e431b3f1a9fa3f57dedbce9efc8768f
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-26 14:55:23 +0100

    Add project files.

---
+++ VariableScope/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CsharpPrograms
+{
+    class Program
+    {
+        public int i;
+
+        public Program(int x)
+
+        {
+            i = x;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program(x);
+            Console.WriteLine(obj.i);
+        }
+    }
+}
